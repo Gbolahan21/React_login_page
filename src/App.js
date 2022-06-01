4cce282cd535cb8d8a434422ef8b0268c8f41ee1
Gbolahan21 <mohammedsodiq98@gmail.com>
2022-06-01 11:58:45 +0100

    second phase

--- src/App.js
+++ src/App.js
@@ -37,75 +37,75 @@ function App() {
       {({ values, errors, handleSubmit, handleChange, handleBlur }) => {
         return (
           <div className="App">
-          <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit}>
 
-          <div className="row justify-content-center">
-          <div className="col-md-6">
-            <span className="anchor" id="formLogin"></span> 
-            <div className="card card-outline-secondary">
-              <div className="card-header">
-                <h3 className="mb-0">Login</h3>
+              <div className="row justify-content-center">
+                <div className="col-md-6">
+                  <span className="anchor" id="formLogin"></span> 
+                  <div className="card card-outline-secondary">
+                    <div className="card-header">
+                      <h3 className="mb-0">Login</h3>
+                    </div>
+                    <div className="card-body">
+                        <div className="form-group">
+                          <label for="uname1">Email</label> 
+                          <input 
+                          type="email"
+                          name="email"
+                          placeholder="Email"
+                          onBlur={handleBlur}
+                          onChange={handleChange}
+                          value={values.email}
+                          className="form-control" 
+                          id="uname1" required="" />
+                          <br />
+                          <span class="error">
+                            {errors.email}
+                          </span>
+                        </div>
+                        <div class="form-group">
+                          <label>Password</label> 
+                          <input 
+                          type="password"
+                          name="password"
+                          placeholder="Password"
+                          onBlur={handleBlur}
+                          onChange={handleChange}
+                          value={values.password}
+                          autocomplete="new-password" 
+                          class="form-control" id="pwd1" required="" />
+                          <br />
+                          <span class="error">
+                            {errors.password}
+                          </span>
+                        </div>
+                        <div class="form-group">
+                          <label>Confirm Password</label> 
+                          <input 
+                          type="password"
+                          name="changepassword"
+                          placeholder="Confirm Password"
+                          onBlur={handleBlur}
+                          onChange={handleChange}
+                          value={values.changepassword}
+                          autocomplete="new-password" 
+                          class="form-control" id="pwd1" required="" />
+                          <span class="error">
+                            {errors.changepassword}
+                          </span>
+                        </div>
+                        <div className="form-check small">
+                          <label className="form-check-label">
+                            <input className="form-check-input" type="checkbox" /> 
+                            <span>Remember me</span>
+                          </label>
+                        </div>
+                        <button className="btn btn-success btn-lg float-right" type="button">Login</button>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="card-body">
-                  <div className="form-group">
-                    <label for="uname1">Email</label> 
-										<input 
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.email}
-                    className="form-control" 
-                    id="uname1" required="" />
-                    <br />
-                    <span class="error">
-                      {errors.email}
-                    </span>
-                  </div>
-                  <div class="form-group">
-                    <label>Password</label> 
-										<input 
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.password}
-                    autocomplete="new-password" 
-                    class="form-control" id="pwd1" required="" />
-                    <br />
-                    <span class="error">
-                      {errors.password}
-                    </span>
-                  </div>
-                  <div class="form-group">
-                    <label>Confirm Password</label> 
-										<input 
-                    type="password"
-                    name="changepassword"
-                    placeholder="Confirm Password"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.changepassword}
-                    autocomplete="new-password" 
-                    class="form-control" id="pwd1" required="" />
-                     <span class="error">
-                      {errors.changepassword}
-                    </span>
-                  </div>
-                  <div className="form-check small">
-                    <label className="form-check-label">
-											<input className="form-check-input" type="checkbox" /> 
-											<span>Remember me</span>
-                    </label>
-                  </div>
-									<button className="btn btn-success btn-lg float-right" type="button">Login</button>
-              </div>
-            </div>
-          </div>
-        </div>
-          </form>
+            </form>
           </div>
         );
       }}
